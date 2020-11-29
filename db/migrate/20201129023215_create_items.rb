1ec6182825a89f4c5663ccef6473b92fe3621ebc
johntakk <takyoshimura32@gmail.com>
2020-11-29 12:25:31 +0900

    item table修正

--- db/migrate/20201129023215_create_items.rb
+++ db/migrate/20201129023215_create_items.rb
@@ -1,7 +1,17 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,           null: false, default: ""
+      t.string :explanation,           null: false, default: ""
+      t.string :category,           null: false, default: ""
+      t.string :status,           null: false, default: ""
+      t.string :description,           null: false, default: ""
+      t.string :delivery_term,           null: false, default: ""
+      t.string :delivery_from,           null: false, default: ""
+      t.string :delivery_leadtime,           null: false, default: ""
+      t.string :price,           null: false, default: ""
+      t.string :handling_fee,           null: false, default: ""
+      t.string :profit,           null: false, default: ""
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_102249) do
+ActiveRecord::Schema.define(version: 2020_11_29_023215) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "explanation", default: "", null: false
+    t.string "category", default: "", null: false
+    t.string "status", default: "", null: false
+    t.string "description", default: "", null: false
+    t.string "delivery_term", default: "", null: false
+    t.string "delivery_from", default: "", null: false
+    t.string "delivery_leadtime", default: "", null: false
+    t.string "price", default: "", null: false
+    t.string "handling_fee", default: "", null: false
+    t.string "profit", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
