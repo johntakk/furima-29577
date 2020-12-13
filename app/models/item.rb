025d0d813a2a4729d8e6dfa3c8e48e0d4d627b33
johntakk <takyoshimura32@gmail.com>
2020-12-13 12:04:15 +0900

    商品出品機能のvalidates設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   has_one_attached :image
+  validates :image, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1 } 
@@ -9,6 +10,8 @@ class Item < ApplicationRecord
   validates :delivery_term_id, numericality: { other_than: 1 } 
   validates :delivery_from_id, numericality: { other_than: 1 } 
   validates :delivery_leadtime_id, numericality: { other_than: 1 } 
+  validates :price, presence: true
+  validates_inclusion_of :price, in: 300..9999999
   belongs_to :category
   belongs_to :delivery_from
   belongs_to :delivery_leadtime
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品' do
+    it '画像が無いと出品できない' do
+      @item.image = ''
+      @user.valid?
+      expect(@item.errors.full_messages).to include("image can't be blank")
+    end
+  end
 end
