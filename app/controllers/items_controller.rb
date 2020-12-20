e5a5ee601ce543c80771f60426007dba8431bf47
johntakk <takyoshimura32@gmail.com>
2020-12-20 12:31:06 +0900

    rev1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy, :edit, :update]
 
   def index
-    @item = Item.all
+    @items = Item.all
   end
 
   def new
