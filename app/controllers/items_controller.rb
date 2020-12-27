ff06d6252dbbc00c5af7e9b2b99477b764b8e1f0
johntakk <takyoshimura32@gmail.com>
2020-12-27 15:20:01 +0900

    update action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,8 +27,13 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :new
+    end
   end
 
   def show
