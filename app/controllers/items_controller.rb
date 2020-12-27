bbcb06eecd702dbb141f3e40500a9bbd59682126
johntakk <takyoshimura32@gmail.com>
2020-12-27 12:25:35 +0900

    edit action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,12 +19,16 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    item = Item.find(params[:id])
+    item.update(item_params)
   end
 
   def show
