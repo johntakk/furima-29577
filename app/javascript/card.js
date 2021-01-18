1b78fa1dbdc249145d5955cce237304fd2adc1ca
johntakk <takyoshimura32@gmail.com>
2021-01-18 12:24:35 +0900

    カードの情報のトークン化終了

---
+++ app/javascript/card.js
@@ -0,0 +1,27 @@
+const pay = () => {
+  Payjp.setPublicKey("pk_test_9bb47b9ac3a7bcbdf3a48635"); // PAY.JPテスト公開鍵
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order[number]"),
+      cvc: formData.get("order[cvc]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`
+    };
+    console.log(card)
+
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
